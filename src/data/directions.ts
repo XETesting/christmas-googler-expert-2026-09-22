67c29b7d07e247c6d1ba033cc91e43aaae190615
jeremymyslowski <49408039+jeremymyslowski@users.noreply.github.com>
2026-09-22 09:47:43 -0400

    Add header, layout, season logic, directions, and sitemap

---
+++ src/data/directions.ts
@@ -0,0 +1,26 @@
+export const directions = {
+  fromNorth: {
+    title: "From the North",
+    steps: [
+      "Take I-81 South to Nuangola Exit 159.",
+      "At end of ramp make a left. Large blue \u201cHelen & Ed\u2019s Tree Farm\u201d directional sign.",
+      "Proceed to stop. Another directional sign. Make a right.",
+      "Proceed to second paved road to left which is Saint Mary\u2019s Road. Another directional sign. Make left.",
+      "Proceed to stop. Straight through for 1.5 miles you will see the tree farm with the big green barn.",
+      "Park in the parking lot adjacent to the barn.",
+    ],
+  },
+  fromSouth: {
+    title: "From the South",
+    steps: [
+      "Take I-81 North to Dorrance Exit 155.",
+      "At end of ramp make a left. Large blue \u201cHelen & Ed\u2019s Tree Farm\u201d directional sign.",
+      "The road is very curvy \u2014 stay on the main road for about 4 miles.",
+      "Pass Andy\u2019s Gas station on the right; proceed about one half mile more to the four-way stop.",
+      "Make left onto Saint Mary\u2019s Road.",
+      "After 1.5 miles you will see the tree farm with the big green barn.",
+      "Park in the parking lot adjacent to the barn.",
+    ],
+  },
+  note: "Located in Dorrance Township, about 5 miles west of the I-81 Nuangola Exit 159.",
+};
